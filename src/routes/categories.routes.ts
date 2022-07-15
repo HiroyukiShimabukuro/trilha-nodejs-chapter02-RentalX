5c3842b12899bdb23fdcbf1dcea8ef5c0f294c27
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-07-15 03:26:23 -0300

    chapter 2 - NodeJS - creating API

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express'
+import { v4 as uuidV4 } from 'uuid';
+import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categories : Category[] = [];
+const categoriesRepository = new CategoriesRepository();
+
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+})
+
+categoriesRoutes.get("/", (request, response) =>{
+  const all = categoriesRepository.list();
+
+  return response.json(all);
+})
+
+export { categoriesRoutes }
