0180f5f082c0f137582dc6a8428e3520a8e4d0d1
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-07-18 03:19:16 -0300

    learning about SOLID SRP-Single Responsability Principle

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { v4 as uuidV4 } from 'uuid';
 import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categories : Category[] = [];
@@ -11,13 +12,9 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if(categoryAlreadyExists){
-    return response.status(400).json({message: "Category already exists!"});
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  
+  createCategoryService.execute({ name, description})
 
   return response.status(201).send();
 })
