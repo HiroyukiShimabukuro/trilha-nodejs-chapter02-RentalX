e1c963b19c099ee14c12324be858bcf4afe78e74
HiroyukiShimabukuro <62634476+HiroyukiShimabukuro@users.noreply.github.com>
2022-08-22 17:29:11 -0300

    changing category repository

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,14 +1,16 @@
-import { Category } from '../../model/Category';
+import { getRepository, Repository } from 'typeorm';
+import { Category } from '../../entities/Category';
 import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository';
 
 
 class CategoriesRepository implements ICategoriesRepository{
-  private categories: Category[];
+  private repository : Repository<Category>;
 
   private static INSTANCE: CategoriesRepository;
 
   private constructor(){
-    this.categories = [];
+
+    this.repository = getRepository(Category);
   }
 
   public static getInstance() : CategoriesRepository{
@@ -18,8 +20,11 @@ class CategoriesRepository implements ICategoriesRepository{
     return CategoriesRepository.INSTANCE;
   }
   
-  create({name, description} : ICreateCategoryDTO): void{
-    const category = new Category(); 
+  async create({name, description} : ICreateCategoryDTO): void{
+    const category = this.repository.create({
+      name,
+      description
+    }); 
 
     Object.assign(category, {
       name,
@@ -27,7 +32,7 @@ class CategoriesRepository implements ICategoriesRepository{
       created_at: new Date(),
     }) 
   
-    this.categories.push(category)
+    await this.repository.save();
   }
 
   list(): Category[]{
