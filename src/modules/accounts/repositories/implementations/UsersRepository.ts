0923b9d375181db8f4dd5f74b4314867aa862951
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-12-23 17:40:23 -0300

    create sessions, verify authentication

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -3,30 +3,38 @@ import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { User } from "../../entities/User";
 import { IUserRepository } from "../IUsersRepository";
 
-class UsersRepository implements IUserRepository{
-    private repository: Repository<User>;
+class UsersRepository implements IUserRepository {
+  private repository: Repository<User>;
 
-    constructor(){
-        this.repository = getRepository(User);
-    }
+  constructor() {
+    this.repository = getRepository(User);
+  }
 
-    async create({name, email, driver_license, password}: ICreateUserDTO): Promise<void> {
-        const user = this.repository.create({
-            name,
-            email, 
-            driver_license,
-            password
-        });
+  async create({
+    name,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      driver_license,
+      password,
+    });
 
-        await this.repository.save(user);
+    await this.repository.save(user);
+  }
 
-    }
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
 
-    async findByEmail(email: string): Promise<User> {
-        const user = await this.repository.findOne({email});
-        return user;
-    }
-
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+    return user;
+  }
 }
 
 export { UsersRepository };
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -5,27 +5,32 @@ import { IUserRepository } from "../../repositories/IUsersRepository";
 
 @injectable()
 class CreateUserUseCase {
-    constructor(
-        @inject('UsersRepository')
-        private usersRepository: IUserRepository
-    ){}
-    
-    async execute({name, email, password, driver_license}: ICreateUserDTO): Promise<void>{
-       const passwordHash = await hash(password, 8);
-       
-       const userAlreadyExists = await this.usersRepository.findByEmail(email);
-    
-       if(userAlreadyExists){
-        throw new Error("User already exists");
-       }
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUserRepository
+  ) {}
 
-        await this.usersRepository.create({
-            name,
-            email, 
-            password: passwordHash, 
-            driver_license
-        });
+  async execute({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
     }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license,
+    });
+  }
 }
 
-export { CreateUserUseCase }
+export { CreateUserUseCase };
